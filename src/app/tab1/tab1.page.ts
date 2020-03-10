d06ed7d9d6e05824875c432cccece36c513e9cfe
unknown <el.didi@hotmail.com>
2020-03-10 08:46:24 -0300

    Se agrego conexion a la API y se listan Autorizaciones

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  
+  constructor(private http: HttpClient) {}
 
-  constructor() {}
+  public resultadosArray : any = null;;
 
+   ngOnInit() {
+    this.listarTodasLasAutorizaciones();
+  }
+
+
+
+  listarTodasLasAutorizaciones(){
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Accept':  'application/json;profile=urn:org.apache.isis/v1',
+        'Authorization': 'Basic YWRtaW46YWRtaW4=',
+      })
+    }
+    const URL = 'http://192.168.1.100:8080/restful/services/Autorizacion/actions/listAll/invoke';
+    this.http.get(URL, httpOptions)
+    .subscribe(resultados => (this.resultadosArray = resultados)
+    );
+      
+  }
+
+  filterItemsOfType(){
+    return this.resultadosArray.filter(resultado => resultado.titulo != null);
+  }
 }
